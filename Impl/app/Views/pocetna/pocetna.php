e5b41a8c292e0b2ab43efdbe4573536dfc6922cf
pajser7256 <pajser7256@gmail.com>
2020-05-12 00:36:05 +0200

    index slike path

--- Impl/app/Views/pocetna/pocetna.php
+++ Impl/app/Views/pocetna/pocetna.php
@@ -1,77 +1,93 @@
 <section class="banner homePageBanner">
     <div class="slider homePageSlider">
-        <div id="slider" class="carousel slide carousel-fade" data-ride="carousel">        
+        <div id="slider" class="carousel slide carousel-fade" data-ride="carousel">
             <div class="carousel-inner">
+
                 <div class="carousel-item active">
-                    <img src="http://localhost/project-bookking/assets/images/book1.jpg" class="d-block w-100 homePageImg" alt="...">
-                        <div class="carousel-caption">
-                            <p class="homePageP"></p>
-                        </div>
+                    <img src="<?php echo base_url('/assets/images/book1.jpg'); ?>" class="d-block w-100 homePageImg" alt="...">
+                    <div class="carousel-caption">
+                        <p class="homePageP"></p>
                     </div>
-                
-                <div class="carousel-item">
-                    <img src="http://localhost/project-bookking/assets/images/book2.jpg" class="d-block w-100 homePageImg" alt="...">
-                        <div class="carousel-caption">       
-                            <p class="homePageP"><!--Tekst--></p>
-                        </div>
-                </div>
-                
-                <div class="carousel-item">
-                    <img src="http://localhost/project-bookking/assets/images/book3.jpg" class="d-block w-100 homePageImg" alt="...">
-                        <div class="carousel-caption">       
-                            <p class="homePageP"><!--Tekst--></p>
-                        </div>
                 </div>
 
                 <div class="carousel-item">
-                    <img src="http://localhost/project-bookking/assets/images/book4.jpg" class="d-block w-100 homePageImg" alt="...">
-                        <div class="carousel-caption">       
-                            <p class="homePageP"><!--Tekst--></p>
-                        </div>
+                    <img src="<?php echo base_url('/assets/images/book2.jpg'); ?>" class="d-block w-100 homePageImg" alt="...">
+                    <div class="carousel-caption">
+                        <p class="homePageP">
+                            <!--Tekst-->
+                        </p>
+                    </div>
                 </div>
 
                 <div class="carousel-item">
-                    <img src="http://localhost/project-bookking/assets/images/book5.jpg" class="d-block w-100 homePageImg" alt="...">
-                        <div class="carousel-caption">       
-                            <p class="homePageP"><!--Tekst--></p>
-                        </div>
+                    <img src="<?php echo base_url('/assets/images/book3.jpg'); ?>" class="d-block w-100 homePageImg" alt="...">
+                    <div class="carousel-caption">
+                        <p class="homePageP">
+                            <!--Tekst-->
+                        </p>
+                    </div>
                 </div>
 
-                
                 <div class="carousel-item">
-                    <img src="http://localhost/project-bookking/assets/images/book6.jpg" class="d-block w-100 homePageImg" alt="...">
-                        <div class="carousel-caption">       
-                            <p class="homePageP"><!--Tekst--></p>
-                        </div>
+                    <img src="<?php echo base_url('/assets/images/book4.jpg'); ?>" class="d-block w-100 homePageImg" alt="...">
+                    <div class="carousel-caption">
+                        <p class="homePageP">
+                            <!--Tekst-->
+                        </p>
+                    </div>
                 </div>
 
-                
                 <div class="carousel-item">
-                    <img src="http://localhost/project-bookking/assets/images/book7.jpg" class="d-block w-100 homePageImg" alt="...">
-                        <div class="carousel-caption">       
-                            <p class="homePageP"><!--Tekst--></p>
-                        </div>
+                    <img src="<?php echo base_url('/assets/images/book5.jpg'); ?>" class="d-block w-100 homePageImg" alt="...">
+                    <div class="carousel-caption">
+                        <p class="homePageP">
+                            <!--Tekst-->
+                        </p>
+                    </div>
                 </div>
-                
+
+
                 <div class="carousel-item">
-                    <img src="http://localhost/project-bookking/assets/images/book8.jpg" class="d-block w-100 homePageImg" alt="...">
-                        <div class="carousel-caption">       
-                            <p class="homePageP"><!--Tekst--></p>
-                        </div>
+                    <img src="<?php echo base_url('/assets/images/book6.jpg'); ?>" class="d-block w-100 homePageImg" alt="...">
+                    <div class="carousel-caption">
+                        <p class="homePageP">
+                            <!--Tekst-->
+                        </p>
+                    </div>
                 </div>
-                
+
+
+                <div class="carousel-item">
+                    <img src="<?php echo base_url('/assets/images/book7.jpg'); ?>" class="d-block w-100 homePageImg" alt="...">
+                    <div class="carousel-caption">
+                        <p class="homePageP">
+                            <!--Tekst-->
+                        </p>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <img src="<?php echo base_url('/assets/images/book8.jpg'); ?>" class="d-block w-100 homePageImg" alt="...">
+                    <div class="carousel-caption">
+                        <p class="homePageP">
+                            <!--Tekst-->
+                        </p>
+                    </div>
+                </div>
+
+
             </div>
-                
-                <ol class="carousel-indicators">
-                        <li data-target="#slider" data-slide-to="0" class="active"></li>
-                        <li data-target="#slider" data-slide-to="1"></li>
-                        <li data-target="#slider" data-slide-to="2"></li>
-                        <li data-target="#slider" data-slide-to="3"></li>
-                        <li data-target="#slider" data-slide-to="4"></li>
-                        <li data-target="#slider" data-slide-to="5"></li>
-                        <li data-target="#slider" data-slide-to="6"></li>
-                        <li data-target="#slider" data-slide-to="7"></li>
-                </ol>
+
+            <ol class="carousel-indicators">
+                <li data-target="#slider" data-slide-to="0" class="active"></li>
+                <li data-target="#slider" data-slide-to="1"></li>
+                <li data-target="#slider" data-slide-to="2"></li>
+                <li data-target="#slider" data-slide-to="3"></li>
+                <li data-target="#slider" data-slide-to="4"></li>
+                <li data-target="#slider" data-slide-to="5"></li>
+                <li data-target="#slider" data-slide-to="6"></li>
+                <li data-target="#slider" data-slide-to="7"></li>
+            </ol>
         </div>
     </div>
 
